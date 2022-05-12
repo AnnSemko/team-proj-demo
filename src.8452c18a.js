deabe7936899d753e832109b3eb4614ce509743f
AnnSemko <95497851+AnnSemko@users.noreply.github.com>
2022-05-12 17:02:57 +0000

    Deploying to gh-pages from @ AnnSemko/team-proj-demo@b39933a8452ee825328efbcb26169ee70787079e 🚀

--- src.8452c18a.js
+++ src.8452c18a.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-proj-demo/src.616dfe55.js.map
+//# sourceMappingURL=/team-proj-demo/src.8452c18a.js.map
